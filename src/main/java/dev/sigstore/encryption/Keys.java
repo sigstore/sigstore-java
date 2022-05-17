69af800cee93d612b7e1970a9e88856ecf4a3dc3
Patrick Flynn <patrick@chainguard.dev>
2022-05-17 13:05:43 -0400

    add some javadoc
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- src/main/java/dev/sigstore/encryption/Keys.java
+++ src/main/java/dev/sigstore/encryption/Keys.java
@@ -15,6 +15,14 @@
  */
 package dev.sigstore.encryption;
 
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.ECKeyParameters;
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,17 +33,18 @@ import java.security.PublicKey;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
-import org.bouncycastle.crypto.params.ECKeyParameters;
-import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
-import org.bouncycastle.crypto.params.RSAKeyParameters;
-import org.bouncycastle.crypto.util.PublicKeyFactory;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
 
 /** For internal use. Key related utility functions. */
 public class Keys {
 
+  /**
+   * Takes a PEM formatted public key in bytes and constructs a {@code PublicKey} with it.
+   *
+   * This method supports the follow public key algorithms: RSA, EdDSA, EC.
+   *
+   * @throws InvalidKeySpecException if the PEM does not contain just one public key.
+   * @throws NoSuchAlgorithmException if the public key is using an unsupported algorithm.
+   */
   public static PublicKey parsePublicKey(byte[] keyBytes)
       throws InvalidKeySpecException, IOException, NoSuchAlgorithmException {
     PemReader pemReader =
