3eaf236052fe21f2c37161da6798d3d51e135f60
Patrick Flynn <patrick@chainguard.dev>
2022-05-17 13:08:13 -0400

    spotless
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- src/main/java/dev/sigstore/encryption/Keys.java
+++ src/main/java/dev/sigstore/encryption/Keys.java
@@ -15,14 +15,6 @@
  */
 package dev.sigstore.encryption;
 
-import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
-import org.bouncycastle.crypto.params.ECKeyParameters;
-import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
-import org.bouncycastle.crypto.params.RSAKeyParameters;
-import org.bouncycastle.crypto.util.PublicKeyFactory;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -33,6 +25,13 @@ import java.security.PublicKey;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.ECKeyParameters;
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 
 /** For internal use. Key related utility functions. */
 public class Keys {
@@ -40,7 +39,7 @@ public class Keys {
   /**
    * Takes a PEM formatted public key in bytes and constructs a {@code PublicKey} with it.
    *
-   * This method supports the follow public key algorithms: RSA, EdDSA, EC.
+   * <p>This method supports the follow public key algorithms: RSA, EdDSA, EC.
    *
    * @throws InvalidKeySpecException if the PEM does not contain just one public key.
    * @throws NoSuchAlgorithmException if the public key is using an unsupported algorithm.
