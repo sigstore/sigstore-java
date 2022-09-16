fbcf87c4fb58e832c0c0237c4eb232c0452a6e6b
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2022-09-16 11:37:19 +0300

    Fixup EnableIfOidcExistsCondition condition for running tests locally
    Signed-off-by: Vladimir Sitnikov <sitnikov.vladimir@gmail.com>

--- build-logic/jvm/src/main/kotlin/build-logic.testing.gradle.kts
+++ build-logic/jvm/src/main/kotlin/build-logic.testing.gradle.kts
@@ -1,6 +1,5 @@
 tasks.withType<Test>().configureEach {
     if (project.hasProperty("skipOidc")) {
-        println("skipOidc: ${project.findProperty("skipOidc")}")
         systemProperty("sigstore-java.test.skipOidc", project.findProperty("skipOidc")!!)
     }
 }
