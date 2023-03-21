27a83078723f955330655fb4ffa9ccfb88f3fae6
Arthur Chan <arthur.chan@adalogics.com>
2023-03-21 21:02:28 +0000

    Refactor new fuzzers to gradle submodule
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- fuzzing/src/main/java/fuzzing/CertificateEntryFuzzer.java
+++ fuzzing/src/main/java/fuzzing/CertificateEntryFuzzer.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package fuzzing;
+
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
 import java.io.ByteArrayInputStream;
