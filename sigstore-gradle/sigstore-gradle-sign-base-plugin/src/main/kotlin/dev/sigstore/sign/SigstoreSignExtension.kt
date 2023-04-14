81618eef9cfbc87b9a5bacac322f537ec12cb1d1
Appu <appu@google.com>
2023-04-14 09:27:09 -0400

    Revert "chore: set version to 0.4.2 for proper release of sigstore-sign Gradle plugin"
    Intentionally commited 0.4.2 for release, move back to 0.5.0
    
    Signed-off-by: Appu Goundan <appu@google.com>

--- sigstore-gradle/sigstore-gradle-sign-base-plugin/src/main/kotlin/dev/sigstore/sign/SigstoreSignExtension.kt
+++ sigstore-gradle/sigstore-gradle-sign-base-plugin/src/main/kotlin/dev/sigstore/sign/SigstoreSignExtension.kt
@@ -39,7 +39,7 @@ abstract class SigstoreSignExtension(private val project: Project) {
     abstract val sigstoreJavaVersion : Property<String>
 
     init {
-        sigstoreJavaVersion.convention("0.4.0")
+        sigstoreJavaVersion.convention("0.5.0")
         (this as ExtensionAware).extensions.create<OidcClientExtension>(
             "oidcClient",
             project.objects,
