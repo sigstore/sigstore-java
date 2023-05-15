263b7fb6d1bce5bf76378db0a766e1655f6544bc
Arthur Chan <arthur.chan@adalogics.com>
2023-05-15 13:22:46 +0000

    Remove unused dependencies
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- fuzzing/build.gradle.kts
+++ fuzzing/build.gradle.kts
@@ -8,16 +8,7 @@ repositories {
 
 dependencies {
     implementation(project(":sigstore-java"))
-
-    implementation(platform("io.grpc:grpc-bom:1.46.0"))
-    implementation("io.grpc:grpc-protobuf")
-
     implementation("com.code-intelligence:jazzer-api:0.16.1")
-    implementation("com.google.code.gson:gson:2.8.9")
-    implementation("com.google.guava:guava:31.1-jre")
-    implementation("com.squareup.okhttp3:mockwebserver:4.9.3")
-    implementation("no.nav.security:mock-oauth2-server:0.5.8")
-    implementation("net.sourceforge.htmlunit:htmlunit:2.70.0")
     implementation("commons-io:commons-io:2.11.0")
 }
 
