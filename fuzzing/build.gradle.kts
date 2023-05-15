65e2e404f800f704c4b522a2dabfec04785c3e63
Arthur Chan <arthur.chan@adalogics.com>
2023-05-15 13:45:59 +0000

    Remove unsued dependencies Move file copy action to gradle build file
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- fuzzing/build.gradle.kts
+++ fuzzing/build.gradle.kts
@@ -8,17 +8,10 @@ repositories {
 
 dependencies {
     implementation(project(":sigstore-java"))
-
-    implementation(platform("io.grpc:grpc-bom:1.46.0"))
-    implementation("io.grpc:grpc-protobuf")
-
     implementation("com.code-intelligence:jazzer-api:0.16.1")
-    implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.google.guava:guava:31.1-jre")
-    implementation("com.squareup.okhttp3:mockwebserver:4.9.3")
     implementation("no.nav.security:mock-oauth2-server:0.5.8")
     implementation("net.sourceforge.htmlunit:htmlunit:2.70.0")
-    implementation("commons-io:commons-io:2.11.0")
 }
 
 // copy to the fuzzing builder's output directory. This is an existing directory with
@@ -27,4 +20,6 @@ tasks.register<Copy>("copyToFuzzOut") {
     dependsOn(tasks.build)
     into(project.findProperty("fuzzOut") ?: project.layout.buildDirectory.dir("fuzzOut"))
     from(sourceSets.main.get().runtimeClasspath)
+    from(project(":sigstore-java").layout.projectDirectory.dir("src/test/resources/dev/sigstore/oidc/server/config.json"))
+    rename("config.json", "oidc-config.json")
 }
