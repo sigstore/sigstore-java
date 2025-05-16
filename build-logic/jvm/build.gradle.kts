7f3f66ebdd5c409e92de46d9f840606748a25b09
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-05-16 17:41:43 +0300

    chore: migrate to Kotlin Dokka 2.0
    Signed-off-by: Vladimir Sitnikov <sitnikov.vladimir@gmail.com>

--- build-logic/jvm/build.gradle.kts
+++ build-logic/jvm/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("com.github.vlsi.gradle-extensions:com.github.vlsi.gradle-extensions.gradle.plugin:1.90")
     implementation("de.thetaphi.forbiddenapis:de.thetaphi.forbiddenapis.gradle.plugin:3.9")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
-    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.0.0")
+    implementation("org.jetbrains.dokka-javadoc:org.jetbrains.dokka-javadoc.gradle.plugin:2.0.0")
     implementation("com.github.autostyle:com.github.autostyle.gradle.plugin:4.0")
     implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.2.0")
 }
