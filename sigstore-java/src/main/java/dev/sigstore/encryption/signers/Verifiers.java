085b33e6ec31269f3d56955351977a64d22eff46
Aaron Lew <64337293+aaronlew02@users.noreply.github.com>
2025-06-16 14:15:13 -0400

    Add support for Ed25519 signatures
    Signed-off-by: Aaron Lew <64337293+aaronlew02@users.noreply.github.com>

--- sigstore-java/src/main/java/dev/sigstore/encryption/signers/Verifiers.java
+++ sigstore-java/src/main/java/dev/sigstore/encryption/signers/Verifiers.java
@@ -17,6 +17,8 @@ package dev.sigstore.encryption.signers;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 
 /** Autodetection for verification algorithms based on public keys used. */
 public class Verifiers {
@@ -28,9 +30,21 @@ public class Verifiers {
     if (publicKey.getAlgorithm().equals("EC") || publicKey.getAlgorithm().equals("ECDSA")) {
       return new EcdsaVerifier(publicKey);
     }
+    if (publicKey.getAlgorithm().equals("Ed25519")) {
+      return new Ed25519Verifier(publicKey);
+    }
+    if (publicKey.getAlgorithm().equals("EdDSA")) {
+      SubjectPublicKeyInfo spki = SubjectPublicKeyInfo.getInstance(publicKey.getEncoded());
+      if (spki.getAlgorithm() != null
+          && new ASN1ObjectIdentifier("1.3.101.112").equals(spki.getAlgorithm().getAlgorithm())) {
+        return new Ed25519Verifier(publicKey);
+      }
+      throw new NoSuchAlgorithmException(
+          "Cannot verify signatures for non-Ed25519 EdDSA key types, this client only supports RSA, ECDSA, and Ed25519 verification");
+    }
     throw new NoSuchAlgorithmException(
         "Cannot verify signatures for key type '"
             + publicKey.getAlgorithm()
-            + "', this client only supports RSA and ECDSA verification");
+            + "', this client only supports RSA, ECDSA, and Ed25519 verification");
   }
 }
--- sigstore-java/src/test/java/dev/sigstore/encryption/signers/VerifiersTest.java
+++ sigstore-java/src/test/java/dev/sigstore/encryption/signers/VerifiersTest.java
@@ -15,33 +15,90 @@
  */
 package dev.sigstore.encryption.signers;
 
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.security.Signature;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
 /** VerifiersTest for failure cases, passing cases are handled in {@link SignerTest}. */
 public class VerifiersTest {
+  private static final byte[] CONTENT = "abcdef".getBytes(StandardCharsets.UTF_8);
 
   @Test
-  public void signatureAlgorithm_unknown() throws Exception {
-    var kp = KeyPairGenerator.getInstance("DSA").generateKeyPair();
-    var exception =
-        Assertions.assertThrows(
-            NoSuchAlgorithmException.class, () -> Verifiers.newVerifier(kp.getPublic()));
-    Assertions.assertEquals(
-        exception.getMessage(),
-        "Cannot verify signatures for key type 'DSA', this client only supports RSA and ECDSA verification");
+  public void verify_ed25519_withBcProvider() throws Exception {
+    var kp = genKeyPairWithBcProvider("ed25519");
+    var signature = genSignature(kp, "ed25519");
+    var verifier = Verifiers.newVerifier(kp.getPublic());
+    Assertions.assertTrue(verifier.verify(CONTENT, signature));
   }
 
   @Test
-  public void signatureAlgorithmForDigests_unknown() throws Exception {
+  public void verify_ed25519_withoutBcProvider() throws Exception {
+    var kp = genKeyPair("ed25519");
+    var signature = genSignature(kp, "ed25519");
+    var verifier = Verifiers.newVerifier(kp.getPublic());
+    Assertions.assertTrue(verifier.verify(CONTENT, signature));
+  }
+
+  @Test
+  public void verify_ed448_withBcProvider() throws Exception {
+    var kp = genKeyPairWithBcProvider("ed448");
+    var signature = genSignature(kp, "ed448");
+    var exception =
+        Assertions.assertThrows(
+            NoSuchAlgorithmException.class, () -> Verifiers.newVerifier(kp.getPublic()));
+    Assertions.assertEquals(
+        "Cannot verify signatures for key type 'Ed448', this client only supports RSA, ECDSA, and Ed25519 verification",
+        exception.getMessage());
+  }
+
+  @Test
+  @EnabledForJreRange(min = JRE.JAVA_15)
+  public void verify_ed448_withoutBcProvider() throws Exception {
+    var kp = genKeyPair("ed448");
+    var signature = genSignature(kp, "ed448");
+    var exception =
+        Assertions.assertThrows(
+            NoSuchAlgorithmException.class, () -> Verifiers.newVerifier(kp.getPublic()));
+    Assertions.assertEquals(
+        "Cannot verify signatures for non-Ed25519 EdDSA key types, this client only supports RSA, ECDSA, and Ed25519 verification",
+        exception.getMessage());
+  }
+
+  @Test
+  public void verify_unknown() throws Exception {
     var kp = KeyPairGenerator.getInstance("DSA").generateKeyPair();
     var exception =
         Assertions.assertThrows(
             NoSuchAlgorithmException.class, () -> Verifiers.newVerifier(kp.getPublic()));
     Assertions.assertEquals(
         exception.getMessage(),
-        "Cannot verify signatures for key type 'DSA', this client only supports RSA and ECDSA verification");
+        "Cannot verify signatures for key type 'DSA', this client only supports RSA, ECDSA, and Ed25519 verification");
+  }
+
+  private KeyPair genKeyPair(String algorithm) throws Exception {
+    KeyPairGenerator kpGen = KeyPairGenerator.getInstance(algorithm);
+    return kpGen.generateKeyPair();
+  }
+
+  private KeyPair genKeyPairWithBcProvider(String algorithm) throws Exception {
+    Security.addProvider(new BouncyCastleProvider());
+
+    KeyPairGenerator kpGen = KeyPairGenerator.getInstance(algorithm, "BC");
+    return kpGen.generateKeyPair();
+  }
+
+  private byte[] genSignature(KeyPair keyPair, String algorithm) throws Exception {
+    Signature signature = Signature.getInstance(algorithm);
+    signature.initSign(keyPair.getPrivate());
+    signature.update(CONTENT);
+    return signature.sign();
   }
 }
