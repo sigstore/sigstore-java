765b6799b65f06842cab2652984cc1049f647186
Aaron Lew <64337293+aaronlew02@users.noreply.github.com>
2025-07-25 17:16:48 -0400

    WIP: Add GraalVM native conformance testing
    Signed-off-by: Aaron Lew <64337293+aaronlew02@users.noreply.github.com>

--- sigstore-cli/build.gradle.kts
+++ sigstore-cli/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("build-logic.java")
     id("application")
+    id("org.graalvm.buildtools.native") version "0.11.0"
 }
 
 repositories {
@@ -15,6 +16,8 @@ dependencies {
     implementation(platform("com.google.oauth-client:google-oauth-client-bom:1.39.0"))
     implementation("com.google.oauth-client:google-oauth-client")
 
+    implementation("com.google.api:gax-grpc:2.68.2")
+
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 }
 
@@ -25,6 +28,25 @@ tasks.compileJava {
 application {
     mainClass.set("dev.sigstore.cli.Sigstore")
 }
+
 tasks.run.configure {
     workingDir = rootProject.projectDir
+}
+
+graalvmNative {
+    binaries.getByName("main") {
+        sharedLibrary.set(false)
+        mainClass.set("dev.sigstore.cli.Sigstore")
+        imageName.set("sigstore-cli")
+
+        resources {
+            autodetection {
+                enabled.set(true)
+            }
+        }
+
+        buildArgs.add("--no-fallback")
+        buildArgs.add("--enable-url-protocols=http,https")
+        buildArgs.add("--initialize-at-run-time=org.bouncycastle,io.grpc.netty.shaded.io.netty")
+    }
 }
