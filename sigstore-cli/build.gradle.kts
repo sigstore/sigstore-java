8bb6fc81b7742c6f9664e5d9e7be1ae3ab8fd33b
Aaron Lew <64337293+aaronlew02@users.noreply.github.com>
2025-07-30 14:09:59 -0400

    WIP: Use HTTP server for conformance testing
    Signed-off-by: Aaron Lew <64337293+aaronlew02@users.noreply.github.com>

--- sigstore-cli/build.gradle.kts
+++ sigstore-cli/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("build-logic.java")
     id("application")
+    id("com.gradleup.shadow") version "9.0.0-rc3"
 }
 
 repositories {
@@ -15,6 +18,11 @@ dependencies {
     implementation(platform("com.google.oauth-client:google-oauth-client-bom:1.39.0"))
     implementation("com.google.oauth-client:google-oauth-client")
 
+    implementation("org.eclipse.jetty:jetty-server:11.0.24")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.24")
+
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 }
 
@@ -25,6 +33,32 @@ tasks.compileJava {
 application {
     mainClass.set("dev.sigstore.cli.Sigstore")
 }
+
+tasks.named("shadowDistTar") {
+    enabled = false
+}
+
+tasks.named("shadowDistZip") {
+    enabled = false
+}
+
 tasks.run.configure {
     workingDir = rootProject.projectDir
+}
+
+tasks.register<ShadowJar>("serverShadowJar") {
+    archiveBaseName.set("sigstore-cli-server")
+    archiveClassifier.set("all")
+    archiveVersion.set("")
+
+    mergeServiceFiles()
+
+    from(sourceSets.main.get().output)
+    configurations = listOf(project.configurations.runtimeClasspath.get())
+
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+
+    manifest {
+        attributes("Main-Class" to "dev.sigstore.cli.ConformanceServer")
+    }
 }
