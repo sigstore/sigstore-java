3fab0dd67041d7ae6b738660fb102cbc028bc723
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-08-07 09:31:38 +0300

    chore: use Java 21 for building sigstore-java
    This change bumps JDK we use when building the project, and it is mainly
    to avoid known bugs in the toolchain.
    
    The resulting binaries should still be compatible with Java 11.
    
    At the same time, the commit skips "./gradlew test" checks for Java 11,
    and we still execute tests with Java 11 via conformance.yml.
    
    It would allow us to bump testing libraries as some of them require Java 17.
    
    Signed-off-by: Vladimir Sitnikov <sitnikov.vladimir@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "sigstore-java-root"
+
+if (JavaVersion.current() < JavaVersion.VERSION_21) {
+    throw UnsupportedOperationException("Please use Java 21+ for launching Gradle when building ${rootProject.name}, the current Java is ${JavaVersion.current().majorVersion}. " +
+            "If you want to execute tests with a different Java version, use -PjdkTestVersion=${JavaVersion.current().majorVersion}")
+}
 
 includeBuild("build-logic-commons")
 includeBuild("build-logic")
